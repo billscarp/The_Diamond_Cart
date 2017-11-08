1ea08f215e49422345de7bd80b8ca0cd168d9cab
billscarp <billscarp@gmail.com>
2017-11-08 16:20:53 -0500

    altering files

--- shopping-cart/routes/index.js
+++ shopping-cart/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. Render functions says sent back response to browswer containing html code of index file and use data between curly brackets  */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Your Shopping Cart' });
 });
 
 module.exports = router;
